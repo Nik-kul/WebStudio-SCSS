a6c8e2085bc77fd8f763edfd8335b5459f404117
Nik-kul <nikitoskul17@gmail.com>
2026-03-24 14:19:17 +0200

    Update mobile-menu.js

--- mobile-menu.js
+++ mobile-menu.js
@@ -3,13 +3,22 @@
   const closeBtn = document.querySelector("[data-menu-close]");
   const menu = document.querySelector("[data-menu]");
 
+  if (!openBtn || !closeBtn || !menu) return;
+
   const toggleMenu = () => {
     menu.classList.toggle("is-hidden");
 
-    document.body.style.overflow =
-      menu.classList.contains("is-hidden") ? "" : "hidden";
+    document.body.style.overflow = menu.classList.contains("is-hidden")
+      ? ""
+      : "hidden";
   };
 
   openBtn.addEventListener("click", toggleMenu);
   closeBtn.addEventListener("click", toggleMenu);
+
+  menu.addEventListener("click", event => {
+    if (event.target === menu) {
+      toggleMenu();
+    }
+  });
 })();
