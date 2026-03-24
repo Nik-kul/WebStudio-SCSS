82c8212fa2a9c785acf7eaaad56f2aa281e43549
Nik-kul <nikitoskul17@gmail.com>
2026-03-24 10:05:03 +0200

    Update mobile-menu.js

--- mobile-menu.js
+++ mobile-menu.js
@@ -1,28 +1,20 @@
 (() => {
-  const mobileMenu = document.querySelector('.js-menu-container');
-  const openMenuBtn = document.querySelector('.js-open-menu');
-  const closeMenuBtn = document.querySelector('.js-close-menu');
-
-  const toggleMenu = () => {
-    const isMenuOpen =
-      openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
-    openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
-    mobileMenu.classList.toggle('is-open');
-
-    const scrollLockMethod = !isMenuOpen
-      ? 'disableBodyScroll'
-      : 'enableBodyScroll';
-    bodyScrollLock[scrollLockMethod](document.body);
+  const refs = {
+    openMenuBtn: document.querySelector(".js-open-menu"),
+    closeMenuBtn: document.querySelector(".js-close-menu"),
+    menu: document.querySelector(".js-menu-container"),
+    backdrop: document.querySelector(".js-backdrop"),
   };
 
-  openMenuBtn.addEventListener('click', toggleMenu);
-  closeMenuBtn.addEventListener('click', toggleMenu);
+  refs.openMenuBtn.addEventListener("click", toggleMenu);
+  refs.closeMenuBtn.addEventListener("click", toggleMenu);
+  refs.backdrop.addEventListener("click", toggleMenu);
 
-  // Close the mobile menu on wider screens if the device orientation changes
-  window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
-    if (!e.matches) return;
-    mobileMenu.classList.remove('is-open');
-    openMenuBtn.setAttribute('aria-expanded', false);
-    bodyScrollLock.enableBodyScroll(document.body);
-  });
+  function toggleMenu() {
+    const isMenuOpen = refs.openMenuBtn.getAttribute("aria-expanded") === "true";
+
+    refs.openMenuBtn.setAttribute("aria-expanded", !isMenuOpen);
+    refs.menu.classList.toggle("is-open");
+    refs.backdrop.classList.toggle("is-open");
+  }
 })();
