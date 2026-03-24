78932384a5f59e45602ef727bf8448d68c9f6f87
Nik-kul <nikitoskul17@gmail.com>
2026-03-24 09:26:15 +0200

    Update mobile-menu.js

--- mobile-menu.js
+++ mobile-menu.js
@@ -1,29 +1,28 @@
 (() => {
-  const refs = {
-    openMenuBtn: document.querySelector("[data-menu-open]"),
-    closeMenuBtn: document.querySelector("[data-menu-close]"),
-    menu: document.querySelector("[data-menu]"),
-  };
-
-  if (!refs.openMenuBtn || !refs.closeMenuBtn || !refs.menu) return;
+  const mobileMenu = document.querySelector('.js-menu-container');
+  const openMenuBtn = document.querySelector('.js-open-menu');
+  const closeMenuBtn = document.querySelector('.js-close-menu');
 
   const toggleMenu = () => {
-    const isMenuOpen = refs.openMenuBtn.getAttribute("aria-expanded") === "true";
+    const isMenuOpen =
+      openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
+    openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
+    mobileMenu.classList.toggle('is-open');
 
-    refs.openMenuBtn.setAttribute("aria-expanded", !isMenuOpen);
-    refs.menu.classList.toggle("is-hidden");
-
-    document.body.style.overflow = refs.menu.classList.contains("is-hidden")
-      ? ""
-      : "hidden";
+    const scrollLockMethod = !isMenuOpen
+      ? 'disableBodyScroll'
+      : 'enableBodyScroll';
+    bodyScrollLock[scrollLockMethod](document.body);
   };
 
-  refs.openMenuBtn.addEventListener("click", toggleMenu);
-  refs.closeMenuBtn.addEventListener("click", toggleMenu);
+  openMenuBtn.addEventListener('click', toggleMenu);
+  closeMenuBtn.addEventListener('click', toggleMenu);
 
-  refs.menu.addEventListener("click", (event) => {
-    if (event.target === refs.menu) {
-      toggleMenu();
-    }
+  // Close the mobile menu on wider screens if the device orientation changes
+  window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
+    if (!e.matches) return;
+    mobileMenu.classList.remove('is-open');
+    openMenuBtn.setAttribute('aria-expanded', false);
+    bodyScrollLock.enableBodyScroll(document.body);
   });
 })();
